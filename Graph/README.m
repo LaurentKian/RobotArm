a77748742e676ff886cab509dcd15b25db53387b
LaurentKian <94679152+LaurentKian@users.noreply.github.com>
2023-01-30 20:03:45 +0800

    Update README.m

--- Graph/README.m
+++ Graph/README.m
@@ -1,1 +1,2 @@
 This folder will be used to conatin graphs of every charpter corresponding to the document.
+![image-20230130200240661](https://cdn.jsdelivr.net/gh/LaurentKian/GraphBed/image-20230130200240661.png)
